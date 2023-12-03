adeea0bd5b29e1ccee7e3a5dd5480a6873f25e3d
Mohamed Benchat <mohamed.benchat@tu-clausthal.de>
2023-12-03 17:36:14 +0100

    add -fopenmp flag to compiler args

--- cython/test1/setup.py
+++ cython/test1/setup.py
@@ -2,10 +2,13 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 ext_modules = [
-        Extension("pi_estimation",
+        Extension(
+            "pi_estimation",
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            extra_compile_args=["-fopenmp"],
+            extra_link_args=["-fopenmp"],
             sources=["pi_estimation.pyx"],
-                libraries=["m"]
+            libraries=["m"]
             )
         ]
 

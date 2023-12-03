883c803d9eca3d6428fc171d2311785124e1276b
Mohamed Benchat <mohamed.benchat@tu-clausthal.de>
2023-12-03 16:20:12 +0100

    add more configuration of the compilation arguments

--- cython/test1/setup.py
+++ cython/test1/setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 
+ext_modules = [
+        Extension("pi_estimation",
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            sources=["pi_estimation.pyx"],
+                libraries=["m"]
+            )
+        ]
 
 setup(
-        ext_modules = cythonize("pi_estimation.pyx")
-
+        name="pi_estimation",
+        ext_modules = cythonize(ext_modules)
     )
